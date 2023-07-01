50efdb5af09f9310d02648484011755f5e37fbe7
iko233 <44130042+iko233@users.noreply.github.com>
2023-07-01 21:12:40 +0800

    Update run.py

--- build/run.py
+++ build/run.py
@@ -2,22 +2,25 @@ import json
 import urllib.request
 import datetime
 import os
+import sys
 
 githubActionPath="/home/runner/work/mc4docker/mc4docker/"
 githubActionRunPath=githubActionPath+"build/"
-jarDownLoadUrl="https://bmclapi2.bangbang93.com/version/#version/server"
-jarDownLoadUrlVersionPlaceHolder="#version"
 jarSavePath=githubActionRunPath+"../system/bin/server.jar"
 getVersionsUrl="http://launchermeta.mojang.com/mc/game/version_manifest.json"
 
 # 检查是否存在大于上次执行的时间版本 节约资源
-def readLastBuildTime():
-    file= open(githubActionRunPath+"./runtime.txt",encoding='utf-8')
-    content=file.read()
-    print("上次系统构建时间为:"+ content+"\n")
-    file.close()
-    lastBuildTime = datetime.datetime.strptime(content,"%Y%m%d")
-    return lastBuildTime
+def readLastBuildTime(timeApi):
+    print("正在获取上次更新时间\n")
+    response = urllib.request.urlopen(timeApi+"mcUpdateTime")
+    lastBuildTimeStr=response.read()
+    print("上次更新时间为:"+lastBuildTimeStr+"\n")
+    lastBuildTime = datetime.datetime.strptime(lastBuildTimeStr,"%Y-%m-%dT%H:%M:%S+00:00")
+    return lastBuildTime,lastBuildTimeStr
+
+def setUpdateTime(timeStr,timeApi):
+    urllib.request.urlopen(timeApi+"set/mcUpdateTime/"+timeStr)
+    return
     
     
 
@@ -32,26 +35,33 @@ def getVersion():
     return lastVersion,versions
 
 
-def main():
-    lastBuildTime = readLastBuildTime()
-
+def main(args):
+    lastBuildTime,lastBuildTimeStr = readLastBuildTime(args[0])
     lastVersion,versions = getVersion()
     print("最后发行正式版本为"+lastVersion+"\n")
     lastVersionTime= lastBuildTime
+    lastVersionTimeStr = lastBuildTimeStr
     for version in versions:
         timeStr=version["time"]
         time = datetime.datetime.strptime(timeStr,"%Y-%m-%dT%H:%M:%S+00:00")
         id=version["id"]
+        jsonurl=version["url"]
+        jsonurlResponse = urllib.request.urlopen(jsonurl)
+        jsonurlResponseJsonStr=jsonurlResponse.read()
+        jsonurlResponseJsonStrDate = json.loads(jsonurlResponseJsonStr)
+        downloadUrl = jsonurlResponseJsonStrDate["downloads"]["server"]["url"]
         if lastVersionTime > time:
             print(id+" 版本已经被构建过 跳过\n")
             lastVersionTime = time
+            lastVersionTimeStr= timeStr
         if lastBuildTime < time:
-            downloadJarFile(jarDownLoadUrl.replace(jarDownLoadUrlVersionPlaceHolder,id))
+            downloadJarFile(downloadUrl,id)
             if id == lastVersion:
                 buildDocker("lastest")
                 pushDocker("lastest")
             buildDocker(id)
             pushDocker(id)
+    setUpdateTime(lastVersionTimeStr ,args[0])
 
         
 def buildDocker(id):
@@ -73,4 +83,4 @@ def downloadJarFile(jarUrl):
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
